27bf89aa6bc6743322bf0d7bdd0048db0885d0a5
Calum Woodward <calum@Calums-MBP.broadband>
2020-08-27 15:45:47 +0100

    updating firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDI0kMmSssDhKPa6H5Ig9zHU6ZEkN_DIfk",
-    authDomain: "twitter-clone-f0855.firebaseapp.com",
-    databaseURL: "https://twitter-clone-f0855.firebaseio.com",
-    projectId: "twitter-clone-f0855",
-    storageBucket: "twitter-clone-f0855.appspot.com",
-    messagingSenderId: "332121056168",
-    appId: "1:332121056168:web:975960cc454fd492a0b39d"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_BASEURL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
